57ae96e426ac38f576273e8b021c6747dbdeecdf
NovaTheProtagonist <marosoliver2003@gmail.com>
2023-10-22 22:47:07 +0200

    Created Hero Class, Implemented Some Required Logic

---
+++ src/main/java/model/hero/Hero.java
@@ -0,0 +1,45 @@
+package model.hero;
+
+public class Hero {
+    private String name;
+    private boolean hasGold;
+    private int arrows;
+    private FacingDirection facingDirection;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean hasGold() {
+        return hasGold;
+    }
+
+    public void grabGold() {
+        this.hasGold = true;
+    }
+
+    public int getArrows() {
+        return arrows;
+    }
+
+    public void decreaseArrowCount() {
+        --arrows;
+    }
+
+    public FacingDirection getFacingDirection() {
+        return facingDirection;
+    }
+
+    public void rotate(Rotation rotation) {
+        switch (rotation) {
+            case RIGHT -> facingDirection = facingDirection.toRight();
+            case LEFT -> facingDirection = facingDirection.toLeft();
+        }
+    }
+}
+
+
