3e755df6a20476ca3ba223b2ee14f1730bbd6bcb
NovaTheProtagonist <marosoliver2003@gmail.com>
2023-10-22 22:45:31 +0200

    Created Enum For Directional Logic
    Added logic to be able to determine which cardinal direction is to the right or left of each cardinal direction

---
+++ src/main/java/model/hero/FacingDirection.java
@@ -0,0 +1,51 @@
+package model.hero;
+
+public enum FacingDirection {
+    NORTH {
+        @Override
+        public FacingDirection toRight() {
+            return FacingDirection.EAST;
+        }
+
+        @Override
+        public FacingDirection toLeft() {
+            return FacingDirection.WEST;
+        }
+    },
+    EAST {
+        @Override
+        public FacingDirection toRight() {
+            return FacingDirection.SOUTH;
+        }
+
+        @Override
+        public FacingDirection toLeft() {
+            return FacingDirection.NORTH;
+        }
+    },
+    SOUTH {
+        @Override
+        public FacingDirection toRight() {
+            return FacingDirection.WEST;
+        }
+
+        @Override
+        public FacingDirection toLeft() {
+            return FacingDirection.EAST;
+        }
+    },
+    WEST {
+        @Override
+        public FacingDirection toRight() {
+            return FacingDirection.NORTH;
+        }
+
+        @Override
+        public FacingDirection toLeft() {
+            return FacingDirection.SOUTH;
+        }
+    };
+
+    public abstract FacingDirection toRight();
+    public abstract FacingDirection toLeft();
+}
