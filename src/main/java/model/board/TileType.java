8e05c836d595840fd9ee283a9a5b7a8f8cbaea13
NovaTheProtagonist <marosoliver2003@gmail.com>
2023-10-22 14:17:54 +0200

    TileType Config

---
+++ src/main/java/model/board/TileType.java
@@ -0,0 +1,20 @@
+package model.board;
+
+public enum TileType {
+    WUMPUS('U'),
+    WALL('W'),
+    GOLD('G'),
+    PIT('P'),
+    EMPTY('_');
+
+    private final char shortName;
+
+    TileType(char shortName) {
+        this.shortName = shortName;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(shortName);
+    }
+}
