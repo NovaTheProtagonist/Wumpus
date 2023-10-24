7f68c340e3dea2ad61e236d745bd8cda119a4450
NovaTheProtagonist <marosoliver2003@gmail.com>
2023-10-24 21:34:21 +0200

    Created Class For CoordinateRep

---
+++ src/main/java/model/coordinate/Coordinate.java
@@ -0,0 +1,36 @@
+package model.coordinate;
+
+import model.hero.FacingDirection;
+
+import java.util.Objects;
+
+public class Coordinate {
+    private final char column;
+    private final int row;
+
+    public Coordinate(char column, int row) {
+        this.column = column;
+        this.row = row;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Coordinate that = (Coordinate) o;
+        return column == that.column && row == that.row;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(column, row);
+    }
+
+    @Override
+    public String toString() {
+        return "Coordinate{" +
+                "column=" + column +
+                ", row=" + row +
+                '}';
+    }
+}
