8a4346a7ee1627a22e3bb804bd0aa3dfe26d77e0
NovaTheProtagonist <marosoliver2003@gmail.com>
2023-10-25 16:15:25 +0200

    Created MenuService Skeleton

---
+++ src/main/java/service/menu/MenuService.java
@@ -0,0 +1,10 @@
+package service.menu;
+
+public interface MenuService {
+    void startGame();
+    void exit();
+    void readFile(String fileName);
+    void loadFromDatabase(String playerName);
+    void saveToDatabase(String playerName, Integer winCount);
+
+}
---
+++ src/main/java/service/menu/MenuServiceImpl.java
@@ -0,0 +1,29 @@
+package service.menu;
+
+public class MenuServiceImpl implements MenuService {
+    @Override
+    public void startGame() {
+
+    }
+
+    @Override
+    public void exit() {
+
+    }
+
+    @Override
+    public void readFile(String fileName) {
+
+    }
+
+    @Override
+    public void loadFromDatabase(String playerName) {
+
+    }
+
+    @Override
+    public void saveToDatabase(String playerName, Integer winCount) {
+        
+    }
+
+}
