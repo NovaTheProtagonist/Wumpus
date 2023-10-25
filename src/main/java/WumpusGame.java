a191bb92a7ea63439f698ac7b41b4ccd05f292bf
NovaTheProtagonist <marosoliver2003@gmail.com>
2023-10-25 17:05:52 +0200

    Implemented Name Scanner

--- src/main/java/WumpusGame.java
+++ src/main/java/WumpusGame.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class WumpusGame {
+    private String playerName;
+
     public void start() {
+        printStartMessage();
+        requestPlayerName();
 
     }
+
+    private void printStartMessage() {
+        System.out.println("Welcome to WumpusLabyrinth\n");
+    }
+
+    private void requestPlayerName() {
+        System.out.println("Please Enter Your Name:\n");
+        Scanner scanner = new Scanner(System.in);
+        playerName = scanner.nextLine();
+        System.out.println("Welcome " + playerName + "!");
+    }
+
 }
