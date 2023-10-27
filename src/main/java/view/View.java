7f0f7321ff3cb337f84a2a5113124508bdc77ba5
NovaTheProtagonist <marosoliver2003@gmail.com>
2023-10-28 00:15:51 +0200

    Added Required Methods For Menu

--- src/main/java/view/View.java
+++ src/main/java/view/View.java
@@ -1,6 +1,13 @@
 package view;
 
+import command.Command;
+
+import java.util.List;
+import java.util.Map;
+
 public interface View {
     void printStartMessage();
     String requestPlayerName();
+    String requestMenuCommand(List<String> commandList);
+    String requestFileName();
 }
