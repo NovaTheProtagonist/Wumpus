257490dec561cdea0b66af39b03c39b1f411c553
NovaTheProtagonist <marosoliver2003@gmail.com>
2023-10-28 16:52:43 +0200

    Created Constructor With ShortID Param

--- src/main/java/model/board/BoardTile.java
+++ src/main/java/model/board/BoardTile.java
@@ -1,7 +1,16 @@
 package model.board;
 
+import java.util.Arrays;
+
 public class BoardTile {
     private TileType type = TileType.EMPTY;
+
+    public BoardTile(char typeID){
+        this.type = Arrays.stream(TileType.values())
+                .filter((tileType -> tileType.getShortName() == typeID))
+                .findAny()
+                .orElseThrow();
+    }
 
     public TileType getType() {
         return type;
