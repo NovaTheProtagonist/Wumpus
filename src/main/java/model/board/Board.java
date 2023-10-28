99d096bd06b57d56b6267d0b1ef19b94f528e58a
NovaTheProtagonist <marosoliver2003@gmail.com>
2023-10-28 16:55:12 +0200

    Implemented Required Functionalities To Read File

--- src/main/java/model/board/Board.java
+++ src/main/java/model/board/Board.java
@@ -1,13 +1,41 @@
 package model.board;
 
+import model.coordinate.Coordinate;
+
 public class Board {
+
+    private final int size;
+
     private final BoardTile[][] boardTiles;
 
-    public Board(int mapSize) {
-        boardTiles = new BoardTile[mapSize][mapSize];
+    private final Coordinate playerStartCoordinate;
+
+    public Board(int size, Coordinate playerStartCoordinate) {
+        this.size = size;
+        boardTiles = new BoardTile[size][size];
+        this.playerStartCoordinate = playerStartCoordinate;
+    }
+    public int getSize() {
+        return size;
     }
 
     public BoardTile getBoardTile(int firstDimension, int secondDimension) {
         return boardTiles[firstDimension][secondDimension];
+    }
+
+    public void setBoardTile(int firstDimension, int secondDimension, char typeID){
+        boardTiles[firstDimension][secondDimension] = new BoardTile(typeID);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                stringBuilder.append(boardTiles[i][j]);
+            }
+            stringBuilder.append("\n");
+        }
+        return stringBuilder.toString();
     }
 }
