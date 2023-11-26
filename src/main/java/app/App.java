71051d83338efa2b8c2b923c564ad700f22e326e
NovaTheProtagonist <marosoliver2003@gmail.com>
2023-11-26 15:36:01 +0100

    Changed Logic For Intialization

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -15,17 +15,20 @@ public class App {
     private static final MenuService menuService = new MenuServiceImpl();
     private static final View view = new ViewImpl();
 
+    private static final WumpusGame wumpusGame = new WumpusGame();
+
     public static void main(String[] args) {
         Map<String, Command> commandMap = assembleCommandMap();
-        WumpusGame game = new WumpusGame(view, menuService, commandMap);
-        game.start();
+        wumpusGame.setView(view);
+        wumpusGame.setCommandMap(commandMap);
+        wumpusGame.start();
     }
 
     private static Map<String, Command> assembleCommandMap() {
         return Map.of(
                 "change name", new SavePlayerNameCommand(menuService, view),
                 "read from file", new ReadFileCommand(menuService, view),
-                "exit", new ExitCommand(menuService)
+                "exit", new ExitCommand(wumpusGame)
         );
     }
 
