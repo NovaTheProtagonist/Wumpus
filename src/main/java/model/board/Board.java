d9d316fd91fc6d4925102939e7bf052d4578a2b3
NovaTheProtagonist <marosoliver2003@gmail.com>
2023-11-29 20:01:20 +0100

    Implemented Hero Spawn

--- src/main/java/model/board/Board.java
+++ src/main/java/model/board/Board.java
@@ -1,6 +1,8 @@
 package model.board;
 
 import model.coordinate.Coordinate;
+import model.hero.Hero;
+import model.hero.Position;
 
 public class Board {
 
@@ -13,7 +15,18 @@ public class Board {
     public Board(int size) {
         this.size = size;
         boardTiles = new BoardTile[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                boardTiles[i][j] = new BoardTile();
+            }
+        }
 
+    }
+
+    public void spawnHero(Hero hero) {
+        Position heroPosition = hero.getPosition();
+        BoardTile boardTile = getBoardTile(heroPosition);
+        boardTile.setOccupied(true);
     }
 
     public int getSize() {
@@ -22,6 +35,10 @@ public class Board {
 
     public BoardTile getBoardTile(int firstDimension, int secondDimension) {
         return boardTiles[firstDimension][secondDimension];
+    }
+
+    public BoardTile getBoardTile(Position position) {
+        return boardTiles[position.getRow()][position.getColumn()];
     }
 
     public void setBoardTile(int firstDimension, int secondDimension, char typeID) {
