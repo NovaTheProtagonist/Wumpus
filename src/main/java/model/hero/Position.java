6ef7042b6d086cdc8f8d398b160f953a165c29be
NovaTheProtagonist <marosoliver2003@gmail.com>
2023-11-29 19:59:59 +0100

    Added Method To Determine Next Position In Direction

--- src/main/java/model/hero/Position.java
+++ src/main/java/model/hero/Position.java
@@ -24,4 +24,33 @@ public class Position {
     public int getRow() {
         return row;
     }
+
+    public Position nextInDirection(FacingDirection facingDirection) {
+        switch (facingDirection) {
+            case NORTH -> {
+                return new Position(column, row - 1);
+            }
+
+            case EAST -> {
+                return new Position(column + 1, row);
+            }
+            case WEST -> {
+                return new Position(column - 1, row);
+            }
+            case SOUTH -> {
+                return new Position(column, row + 1);
+            }
+            default -> {
+                return this;
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Position{" +
+                "column=" + column +
+                ", row=" + row +
+                '}';
+    }
 }
