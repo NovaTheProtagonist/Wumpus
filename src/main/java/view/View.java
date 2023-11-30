46233b2a5f19e20d00864cd84c0d8937fc197400
NovaTheProtagonist <marosoliver2003@gmail.com>
2023-11-30 19:48:28 +0100

    Created Game Phase View Methods

--- src/main/java/view/View.java
+++ src/main/java/view/View.java
@@ -1,10 +1,9 @@
 package view;
 
 import java.util.List;
-import java.util.Optional;
 
 import model.board.Board;
-
+import model.hero.Hero;
 
 
 public interface View {
@@ -13,10 +12,22 @@ public interface View {
     String requestPlayerName();
 
     String requestMenuCommand(List<String> commandList);
+
     String requestGameCommand(List<String> commandList);
+
     String requestFileName();
 
-    void printBoard(Optional<Board> board);
-
     void printCommandNotFound();
+
+    void printBoard(Board board);
+
+    void printError(String errorMessage);
+
+    void printHero(Hero hero);
+
+    void printWin();
+
+    void printLose();
+
+    void printInstructions();
 }
--- src/main/java/view/ViewImpl.java
+++ src/main/java/view/ViewImpl.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import java.util.Scanner;
 
 import model.board.Board;
+import model.hero.Hero;
+import model.hero.Position;
 
 public class ViewImpl implements View {
     @Override
@@ -47,18 +49,52 @@ public class ViewImpl implements View {
         System.out.println("Loading File: " + fileName);
         return fileName;
     }
-
-    @Override
-    public void printBoard(Optional<Board> board) {
-        if (board.isPresent()) {
-            System.out.println(board.get());
-        } else {
-            System.out.println("Unable to read file");
-        }
-    }
-
     @Override
     public void printCommandNotFound() {
         System.out.println("Invalid Command");
+    }
+
+    @Override
+    public void printBoard(Board board) {
+        System.out.println(board);
+    }
+
+    @Override
+    public void printError(String errorMessage) {
+        System.out.println("ERROR: " + errorMessage);
+    }
+
+    @Override
+    public void printHero(Hero hero) {
+        System.out.println(hero.getName());
+        Position heroPosition = hero.getPosition();
+        System.out.println("Position: " + heroPosition.getColumn() + 1 + ", " + heroPosition.getRow() + 1);
+        System.out.println("Facing: " + hero.getFacingDirection().name());
+        System.out.println("Ammo: " + hero.getArrows());
+        System.out.println(hero.hasGold() ? "You are in possession of the Gold" : "You do not possess the Gold");
+        System.out.println();
+    }
+
+    @Override
+    public void printWin() {
+        System.out.println("You have secured the gold! Congratulations, you win!");
+
+    }
+
+    @Override
+    public void printLose() {
+        System.out.println("You have died");
+    }
+
+    @Override
+    public void printInstructions() {
+        System.out.println("The goal of the game is to pick up the gold (G) and bring it back to your starting location (S)!");
+        System.out.println("If you step onto a tile with a pit (P) you lose an arrow unless you don't have any arrows left," +
+                " in this case you die");
+        System.out.println("If you shoot an arrow and it hits a Wumpus (U), the Wumpus (U) will die and you lose an arrow");
+        System.out.println("If your arrow doesn't hit a Wumpus (U) it will break upon hitting a wall (W) " +
+                "resulting in you losing an arrow");
+        System.out.println("If you step onto a tile where a Wumpus (U) resides you shall perish");
+        System.out.println("Good luck!\n");
     }
 }
