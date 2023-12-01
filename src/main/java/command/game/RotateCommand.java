79dc2c293deae3ba45787ebf3146ef7a6f7893ea
NovaTheProtagonist <marosoliver2003@gmail.com>
2023-12-01 15:26:16 +0100

    Implemented Rotation

--- src/main/java/command/game/RotateCommand.java
+++ src/main/java/command/game/RotateCommand.java
@@ -1,8 +1,11 @@
 package command.game;
 
 import command.Command;
+import model.hero.Rotation;
 import service.game.GameService;
 import view.View;
+
+import java.util.Optional;
 
 public class RotateCommand extends GameCommand {
 
@@ -12,6 +15,11 @@ public class RotateCommand extends GameCommand {
 
     @Override
     protected void runBeforePrint() {
-
+        Optional<Rotation> rotation = view.requestRotation();
+        if (rotation.isEmpty()){
+            view.printError("Invalid rotation");
+            return;
+        }
+        gameService.rotatePlayer(rotation.get());
     }
 }
