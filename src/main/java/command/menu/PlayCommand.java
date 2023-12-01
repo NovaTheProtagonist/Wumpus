25cea25a813b6a5c9a62385bed71488a3cef28e5
NovaTheProtagonist <marosoliver2003@gmail.com>
2023-12-01 13:20:03 +0100

    Created Command To Start Game

---
+++ src/main/java/command/menu/PlayCommand.java
@@ -0,0 +1,48 @@
+package command.menu;
+
+import app.GameState;
+import app.WumpusGame;
+import command.Command;
+import model.board.Board;
+import model.hero.Hero;
+import service.game.GameService;
+import service.menu.MenuService;
+import view.View;
+
+import java.util.Optional;
+
+public class PlayCommand implements Command {
+
+    private final View view;
+
+    private final WumpusGame wumpusGame;
+
+    private final MenuService menuService;
+
+    private final GameService gameService;
+    public PlayCommand(View view, WumpusGame wumpusGame, MenuService menuService, GameService gameService) {
+        this.view = view;
+        this.wumpusGame = wumpusGame;
+        this.menuService = menuService;
+        this.gameService = gameService;
+    }
+
+    @Override
+    public void execute() {
+        Optional<Board> board = menuService.getBoard();
+        Optional<Hero> hero = menuService.getHero();
+        if (board.isEmpty() || hero.isEmpty()) {
+            view.printError("You haven't read a game file yet");
+            return;
+        }
+        Board gameBoard = board.get();
+        gameService.setBoard(gameBoard);
+        Hero gameHero = hero.get();
+        gameService.setHero(gameHero);
+        gameService.spawnPlayer();
+        view.printInstructions();
+        view.printHero(gameHero);
+        view.printBoard(gameBoard);
+        wumpusGame.setGameState(GameState.GAME_STATE);
+    }
+}
